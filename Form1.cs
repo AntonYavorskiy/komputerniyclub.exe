c6850930bde1c9a154ca31b0462c37acf23c0d4a
AntonYavorskiy <63713478+AntonYavorskiy@users.noreply.github.com>
2020-04-15 16:29:18 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace vhod
+{
+    public partial class Form1 : Form
+    {
+        private SQLiteConnection DB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DB = new SQLiteConnection("Data Source = users.db; Version=3");
+            DB.Open();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DB.Close();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            if (loginbox.Text == "" || passwordbox.Text == "")
+            MessageBox.Show("Пожалуйста, заполните все поля");
+            if (loginbox.Text != "" && passwordbox.Text != "")
+            {
+                SQLiteCommand CMD = DB.CreateCommand();
+                CMD.CommandText = "select * from accounts where login like @login and password like  @password ";
+                CMD.Parameters.Add("@login", System.Data.DbType.String).Value = loginbox.Text.ToUpper();
+                CMD.Parameters.Add("@password", System.Data.DbType.String).Value = passwordbox.Text.ToUpper();
+                SQLiteDataReader SQL = CMD.ExecuteReader();
+                if (SQL.HasRows)
+                {
+                    while(SQL.Read())
+                    {
+
+                        MessageBox.Show(string.Format("Здравствуйте, {0} {1}", SQL["name"],SQL["fio"] ));
+                        menu m = new menu();
+                        this.Hide();
+                        m.ShowDialog();
+                        this.Show();
+
+                    }
+                }
+                else MessageBox.Show("Вы ввели неверные данные");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            register f = new register();
+            this.Hide();
+            f.ShowDialog();
+            this.Show();
+        }
+    }
+}
---
+++ menu.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace vhod
+{
+    public partial class menu : Form
+    {
+        public menu()
+        {
+            InitializeComponent();
+            timer2.Interval = 660;
+            timer.Interval = 1000;
+            timer.Elapsed += timer1_Tick;
+            timer.Enabled = true;
+        }
+       
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            if (pc1.Enabled == true)
+            {
+                pc1.Enabled = false;
+                pc1.BackColor = Color.Silver;
+
+            }
+            else
+
+               if (pc1.Enabled == false)
+            {
+               
+                pc1.Enabled = true;
+                pc1.BackColor = Color.Chartreuse;
+            }
+
+        }
+ System.Timers.Timer timer = new System.Timers.Timer();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            var str = DateTime.Now.ToString("HH:mm:ss");
+
+            if (label2.InvokeRequired) label2.Invoke(new Action<string>((s) => label2.Text = s), str);
+            else label2.Text = str;
+           
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedIndex)
+            {
+                case 0:
+                    pc1.Enabled = false;
+                    pc1.BackColor = Color.Silver;
+
+                    break;
+                case 1:
+                    MessageBox.Show("Выбран второй");
+                    break;
+            }
+        }
+        public bool b = true;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (b)
+            {
+                this.BackColor = Color.Blue;this.BackColor = Color.Red;
+                
+            }
+            
+            
+            
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    timer2.Start();
+                 
+
+                    break;
+                case 1:
+                    MessageBox.Show("Выбран второй");
+                    break;
+            }
+        }
+
+        private void menu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
